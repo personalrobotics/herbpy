4f1bdca3e66cbfa1521fa1b20a6d6214f33e1b0e
Michael Koval <mkoval@cs.cmu.edu>
2013-03-29 20:14:45 +0000

    Added docstrings to planner.py.

--- src/herbpy/planner.py
+++ src/herbpy/planner.py
@@ -14,16 +14,36 @@ class UnsupportedPlanningError(PlanningError):
 
 class Planner:
     def GetName(self):
+        """
+        Gets the planner's name.
+        @return planner_name
+        """
         return 'unknown planner'
 
     @PlanningMethod
     def PlanToConfiguration(self, goal, **kw_args):
+        """
+        Plan from the robot's current configuration to a goal configuration.
+        @param goal desired configuration
+        @return trajectory
+        """
         raise UnsupportedPlanningError('This planner does not support PlanToConfiguration.')
 
     @PlanningMethod
     def PlanToEndEffectorPose(self, goal_pose):
+        """
+        Plan from the robot's current configuration to a goal end-effector pose.
+        @param goal_pose desired end-effector pose
+        @return trajectory
+        """
         raise UnsupportedPlanningError('This planner does not support PlanToEndEffectorPose.')
 
     @PlanningMethod
     def PlanToEndEffectorOffset(self, direction, distance):
+        """
+        Plan a straight motion in the world frame.
+        @param direction unit vector in the direction of motion
+        @param distance distance along the direction of motion
+        @return trajectory
+        """
         raise UnsupportedPlanningError('This planner does not support PlanToEndEffectorOffset.')
