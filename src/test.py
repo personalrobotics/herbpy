625c8330c9d5f90165b3458ae090d61d0a83a167
Prasanna Velagapudi <pkv@cs.cmu.edu>
2013-03-30 19:31:07 +0000

    Changed test.py to be using real robot again.

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ def look_at_hand(robot, manipulator):
     target = manipulator.GetEndEffectorTransform()[0:3, 3]
     robot.LookAt(target, execute=True)
 
-simulation = True
+simulation = False
 left_home_config = numpy.array([ 2.60, -1.90,  0.00,  2.20,  0.00,  0.00,  0.00 ])
 left_relaxed_config = numpy.array([ 0.55, -1.82,  0.35,  1.87, -2.22, -0.66, -0.98 ])
 right_home_config = numpy.array([ 3.68, -1.90, 0.00, 2.20, 0.00, 0.00, 0.00 ])
