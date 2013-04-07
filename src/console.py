0927f8a66853065df1603f45f503044b9ef6083c
Michael Koval <mkoval@cs.cmu.edu>
2013-04-07 21:56:14 +0000

    Added a "sim" flag to herbpy console.

--- src/console.py
+++ src/console.py
@@ -2,9 +2,15 @@
 Provides a simple console that sets up basic functionality for 
 using herbpy and openravepy.
 """
-import herbpy, openravepy
+import herbpy, numpy, openravepy, sys
 
 if __name__ == "__main__":
     openravepy.RaveInitialize(True, level=openravepy.DebugLevel.Info)
     openravepy.misc.InitOpenRAVELogging();
-    env, robot = herbpy.initialize_real()
+
+    if 'sim' in sys.argv:
+        env, robot = herbpy.initialize_sim()
+        herbpy.logger.info('Initialized in simulation mode.')
+    else:
+        env, robot = herbpy.initialize_real()
+        herbpy.logger.info('Initialized connection with HERB.')
