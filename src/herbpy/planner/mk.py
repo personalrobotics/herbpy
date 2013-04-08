33a0352738375d8c6c5d8d14987cf27089148e80
Michael Koval <mkoval@cs.cmu.edu>
2013-04-08 06:27:15 +0000

    Significantly improved mk planner.

--- src/herbpy/planner/mk.py
+++ src/herbpy/planner/mk.py
@@ -3,13 +3,27 @@ import prrave.tsr
 import planner
 
 def DoNothing(robot):
-    return numpy.zeros(7)
+    return numpy.zeros(robot.GetActiveDOF())
 
-def JointLimitAvoidance(robot):
+# Based on Moslem Kazemi's code from ARM-S.
+def JointLimitAvoidance(robot, limit_tolerance=0.2, gain=50):
     q = robot.GetActiveDOFValues()
     q_min, q_max = robot.GetDOFLimits(robot.GetActiveDOFIndices())
-    constant = (q_max - q_min) / (2 * q.shape[0])
-    return -constant * (q**2 - q_min * q_max) / ((q_max - q)**2 * (q - q_min)**2)
+
+    num_dofs = robot.GetActiveDOF()
+    q_dot = numpy.zeros(num_dofs)
+    for i in xrange(num_dofs):
+        max_limit_dist = q_max[i] - q[i]
+        min_limit_dist = q_min[i] - q[i]
+
+        if max_limit_dist < limit_tolerance:
+            q_dot[i] = -gain * (max_limit_dist - limit_tolerance) ** 2
+        elif min_limit_dist > -limit_tolerance:
+            q_dot[i] =  gain * (min_limit_dist + limit_tolerance) ** 2
+        else:
+            q_dot[i] = 0
+
+    return q_dot
 
 class MKPlanner(planner.Planner):
     def __init__(self, robot):
@@ -19,20 +33,21 @@ class MKPlanner(planner.Planner):
     def GetName(self):
         return 'mk'
 
-    def GetStraightVelocity(self, manip, velocity, initial_hand_pose, nullspace_fn):
+    def GetStraightVelocity(self, manip, velocity, initial_hand_pose, nullspace_fn, step_size):
         current_hand_pose = manip.GetEndEffectorTransform()
+        initial_position = initial_hand_pose[0:3, 3]
+        current_position = current_hand_pose[0:3, 3]
 
-        # Project the position error orthogonal to the velocity of motion. Then
-        # add a constant forward force.
-        #pos_offset = initial_hand_pose[0:3, 3] - current_hand_pose[0:3, 3]
-        #pos_error  = pos_offset - hand_velocity * numpy.dot(pos_offset, hand_velocity)
-        error_pos = velocity 
+        # Simulate a position goal step_size distance further along the velocity vector.
+        moved_already = velocity * numpy.dot(current_position - initial_position, velocity)
+        desired_position = initial_position + moved_already + velocity * step_size
+        error_pos = desired_position - current_position
 
         # Append the desired quaternion to create the error vector. There is a
         # sign ambiguity on quaternions, so we'll always choose the shortest path.
         initial_ori = openravepy.quatFromRotationMatrix(initial_hand_pose)
         current_ori = openravepy.quatFromRotationMatrix(current_hand_pose)
-        choices_ori = [ initial_ori - current_ori, current_ori - initial_ori ]
+        choices_ori = [ current_ori - initial_ori, current_ori + initial_ori ]
         error_ori = min(choices_ori, key=lambda q: numpy.linalg.norm(q))
 
         # Jacobian pseudo-inverse.
@@ -44,13 +59,12 @@ class MKPlanner(planner.Planner):
         # Null-space projector
         nullspace_projector = numpy.eye(jacobian.shape[1]) - numpy.dot(jacobian_pinv, jacobian)
         nullspace_goal = nullspace_fn(self.robot)
-        #print nullspace_projector, nullspace_goal
         pose_error = numpy.hstack((error_pos, error_ori))
         return numpy.dot(jacobian_pinv, pose_error) + numpy.dot(nullspace_projector, nullspace_goal)
 
     def PlanToEndEffectorOffset(self, direction, distance, nullspace=JointLimitAvoidance,
-                                timelimit=0.5, step_size=0.01,
-                                position_tolerance=0.01, angular_tolerance=0.03, **kw_args):
+                                timelimit=2.5, step_size=0.001,
+                                position_tolerance=0.01, angular_tolerance=0.15, **kw_args):
         direction  = numpy.array(direction, dtype='float')
         direction /= numpy.linalg.norm(direction)
 
@@ -75,8 +89,8 @@ class MKPlanner(planner.Planner):
                         raise planner.PlanningError('Reached time limit.')
 
                     # Compute joint velocities using the Jacobian pseudoinverse.
-                    q_dot = self.GetStraightVelocity(manip, direction, initial_pose, nullspace)
-                    q += step_size * q_dot / numpy.linalg.norm(q_dot)
+                    q_dot = self.GetStraightVelocity(manip, direction, initial_pose, nullspace, step_size)
+                    q += q_dot
                     self.robot.SetDOFValues(q, active_dof_indices)
                     traj.Insert(traj.GetNumWaypoints(), q)
 
@@ -108,5 +122,5 @@ class MKPlanner(planner.Planner):
                     hand_pose = manip.GetEndEffectorTransform()
                     displacement = hand_pose[0:3, 3] - initial_pose[0:3, 3]
                     current_distance = numpy.dot(displacement, direction)
-        openravepy.planningutils.RetimeTrajectory(traj)
+
         return traj
