ee17d16e2ca2a5baa9ee017bd99de3930633bd11
Michael Koval <mkoval@cs.cmu.edu>
2013-04-17 21:37:47 +0000

    Added soft-dependency logic.

---
+++ src/herbpy/dependency_manager.py
@@ -0,0 +1,51 @@
+import herbpy, rospkg, os
+
+def append_to_env(variable, new_path, separator=':'):
+    paths = os.environ.get(variable, '')
+    if paths:
+        paths_list = paths.split(separator)
+    else:
+        paths_list = list()
+
+    # Check if we're adding a duplicate entry.
+    duplicate = False
+    new_canonical_path = os.path.realpath(new_path)
+    for path in paths_list: 
+        canonical_path = os.path.realpath(path)
+        if canonical_path == new_canonical_path:
+            duplicate = True
+            break
+
+    # Add the new path to the environmental variable.
+    if not duplicate:
+        paths_list.insert(0, new_path)
+        paths = separator.join(paths_list)
+        os.environ[variable] = paths
+
+    return paths 
+
+def export_paths(package_name):
+    pkg = rospkg.RosPack()
+    try:
+        dependencies = pkg.get_depends(package_name)
+    except rospkg.ResourceNotFound:
+        return False
+
+    plugin_paths = list()
+    data_paths = list()
+    for package in dependencies:
+        # Load the package manifest.
+        try:
+            manifest = pkg.get_manifest(package_name)
+        except rospkg.ResourceNotFound:
+            herbpy.logger.warning('Dependency %s was not found.', package_name)
+            return False
+
+        # Process the OpenRAVE export tags. 
+        for plugin_path in manifest.get_export('openrave', 'plugins'):
+            plugin_paths = append_to_env('OPENRAVE_PLUGINS', plugin_path)
+
+        for data_path in manifest.get_export('openrave', 'data'):
+            data_paths = append_to_env('OPENRAVE_DATA', data_path)
+
+    return True
