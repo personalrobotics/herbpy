a667aa183481046c83a65f6ed7bace6c8c223043
Jennifer King <jeking@andrew.cmu.edu>
2015-04-02 15:21:29 -0400

    Adding push_grasp action to fuze. Fixing grasping to allow tsrlist and render as options to Grasp and PushGrasp actions

--- src/herbpy/action/grasping.py
+++ src/herbpy/action/grasping.py
@@ -1,21 +1,29 @@
 import logging, prpy
 from prpy.action import ActionMethod
+from contextlib import contextmanager
 
 logger = logging.getLogger('herbpy')
 
 @ActionMethod
-def Grasp(robot, obj, manip=None, preshape=[0., 0., 0., 0.], **kw_args):
+def Grasp(robot, obj, manip=None, preshape=[0., 0., 0., 0.], 
+          tsrlist=None, render=True, **kw_args):
     """
     @param robot The robot performing the push grasp
     @param obj The object to push grasp
     @param manip The manipulator to perform the grasp with 
        (if None active manipulator is used)
     @param preshape The grasp preshape for the hand
+    @param tsrlist A list of TSRChain objects to use for planning to grasp pose
+       (if None, the 'grasp' tsr from tsrlibrary is used)
+    @param render Render tsr samples and push direction vectors during planning
     """
-    HerbGrasp(robot, obj,  manip=manip, preshape=preshape)
+    HerbGrasp(robot, obj,  manip=manip, preshape=preshape, 
+              tsrlist=tsrlist, render=render)
 
 @ActionMethod
-def PushGrasp(robot, obj, distance=0.1, manip=None, preshape=[0., 0., 0., 0.], push_required=True, **kw_args):
+def PushGrasp(robot, obj, push_distance=0.1, manip=None, 
+              preshape=[0., 0., 0., 0.], push_required=True, 
+              tsrlist=None, render=True, **kw_args):
     """
     @param robot The robot performing the push grasp
     @param obj The object to push grasp
@@ -26,12 +34,22 @@ def PushGrasp(robot, obj, distance=0.1, manip=None, preshape=[0., 0., 0., 0.], p
        movement cannot be found. If false, continue with grasp even if push 
        cannot be executed.
     @param preshape The grasp preshape for the hand
+    @param tsrlist A list of TSRChain objects to use for planning to grasp pose
+       (if None, the 'grasp' tsr from tsrlibrary is used)
+    @param render Render tsr samples and push direction vectors during planning
     """
-    HerbGrasp(robot, obj,  manip=manip, preshape=preshape, distance=distance)
+    if tsrlist is None:
+        tsrlist = robot.tsrlibrary(obj, 'push_grasp', push_distance=push_distance)
 
-def HerbGrasp(robot, obj, distance=None, manip=None, 
+    HerbGrasp(robot, obj, manip=manip, preshape=preshape, 
+              push_distance=push_distance,
+              tsrlist=tsrlist, render=render)
+
+def HerbGrasp(robot, obj, push_distance=None, manip=None, 
               preshape=[0., 0., 0., 0.], 
               push_required=False, 
+              tsrlist=None,
+              render=True,
               **kw_args):
     """
     @param robot The robot performing the push grasp
@@ -43,6 +61,7 @@ def HerbGrasp(robot, obj, distance=None, manip=None,
     @param push_required If true, throw exception if a plan for the pushing 
        movement cannot be found. If false, continue with grasp even if push 
        cannot be executed. (only used if distance is not None)
+    @param render Render tsr samples and push direction vectors during planning
     """
     if manip is None:
         manip = robot.GetActiveManipulator()
@@ -51,13 +70,48 @@ def HerbGrasp(robot, obj, distance=None, manip=None,
     manip.hand.MoveHand(*preshape)
 
     # Get the grasp tsr
-    grasp_tsr_list = robot.tsrlibrary(obj, 'grasp') 
+    if tsrlist is None:
+        tsrlist = robot.tsrlibrary(obj, 'grasp')
     
     # Plan to the grasp
-    with prpy.viz.RenderTSRList(grasp_tsr_list, robot.GetEnv()):
-        manip.PlanToTSR(grasp_tsr_list)
+    if not render:
+        class RenderNothing():
+            def __init__(self, *args, **kw_args):
+                pass
+            def __enter__(self):
+                pass
+            def __exit__(self, type, value, traceback):
+                pass
 
-    if distance is not None:
+    render_func = prpy.viz.RenderTSRList if render else RenderNothing
+    with render_func(tsrlist, robot.GetEnv()):
+        manip.PlanToTSR(tsrlist)
+
+    if push_distance is not None:
+        ee_in_world = manip.GetEndEffectorTransform()
+        push_direction = ee_in_world[:3,2]
+
+        # Move the object into the hand
+        env = robot.GetEnv()
+        with env:
+            obj_in_world = obj.GetTransform()
+
+        # First move back until collision
+        stepsize = 0.01
+        total_distance = 0.0
+        while not env.CheckCollision(robot, obj) and total_distance <= push_distance:
+            obj_in_world[:3,3] -= stepsize*push_direction
+            total_distance += stepsize
+            with env:
+                obj.SetTransform(obj_in_world)
+            
+        # Then move forward until just out of collision
+        stepsize = 0.001
+        while env.CheckCollision(robot, obj):
+            obj_in_world[:3,3] += stepsize*push_direction
+            with env:
+                obj.SetTransform(obj_in_world)
+
         # Manipulator must be active for grab to work properly
         active_manip = robot.GetActiveManipulator()
         robot.SetActiveManipulator(manip)
@@ -65,13 +119,12 @@ def HerbGrasp(robot, obj, distance=None, manip=None,
         robot.SetActiveManipulator(active_manip)
                 
         # Now execute the straight line movement
-        ee_in_world = manip.GetEndEffectorTransform()
-        push_direction = ee_in_world[:3,2]
-        with prpy.viz.RenderVector(ee_in_world[:3,3], push_direction,
-                                   distance, robot.GetEnv()):
+        render_func = prpy.viz.RenderVector if render else RenderNothing
+        with render_func(ee_in_world[:3,3], push_direction,
+                         push_distance, robot.GetEnv()):
             try:
                 manip.PlanToEndEffectorOffset(direction = push_direction,
-                                              distance = distance,
+                                              distance = push_distance,
                                               **kw_args)
             except PlanningError, e:
                 if push_required:
--- src/herbpy/tsr/fuze.py
+++ src/herbpy/tsr/fuze.py
@@ -1,15 +1,36 @@
 import numpy
 from prpy.tsr.tsrlibrary import TSRFactory
-from prpy.tsr.tsr import *
+from prpy.tsr.tsr import TSR, TSRChain
 
 @TSRFactory('herb', 'fuze_bottle', 'grasp')
 def fuze_grasp(robot, fuze, manip=None):
-    '''
+    """
     @param robot The robot performing the grasp
     @param fuze The fuze to grasp
     @param manip The manipulator to perform the grasp, if None
        the active manipulator on the robot is used
-    '''
+    """
+    return _fuze_grasp(robot, fuze, manip = manip)
+
+@TSRFactory('herb', 'fuze_bottle', 'push_grasp')
+def fuze_grasp(robot, fuze, push_distance = 0.1, manip=None):
+    """
+    @param robot The robot performing the grasp
+    @param fuze The fuze to grasp
+    @param push_distance The distance to push before grasping
+    @param manip The manipulator to perform the grasp, if None
+       the active manipulator on the robot is used
+    """
+    return _fuze_grasp(robot, fuze, push_distance = push_distance, manip = manip)
+
+def _fuze_grasp(robot, fuze, push_distance = 0.0, manip = None):
+    """
+    @param robot The robot performing the grasp
+    @param fuze The fuze to grasp
+    @param push_distance The distance to push before grasping
+    @param manip The manipulator to perform the grasp, if None
+       the active manipulator on the robot is used
+    """
     if manip is None:
         manip_idx = robot.GetActiveManipulatorIndex()
     else:
@@ -18,9 +39,13 @@ def fuze_grasp(robot, fuze, manip=None):
             manip_idx = manip.GetRobot().GetActiveManipulatorIndex()
 
     T0_w = fuze.GetTransform()
-    Tw_e = numpy.array([[ 0., 0., 1., -(0.038 + 0.19)], 
+    ee_to_palm_distance = 0.18
+    default_offset_distance = 0.05 # This is the radius of the fuze
+                                   # plus a little bit
+    total_offset = ee_to_palm_distance + default_offset_distance + push_distance
+    Tw_e = numpy.array([[ 0., 0., 1., -total_offset], 
                         [1., 0., 0., 0.], 
-                        [0., 1., 0., 0.108], 
+                        [0., 1., 0., 0.108], # half of fuze bottle height
                         [0., 0., 0., 1.]])
 
     Bw = numpy.zeros((6,2))
