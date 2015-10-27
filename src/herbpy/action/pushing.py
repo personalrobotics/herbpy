cb54a3206d05a04b0afbfb6bb3715a6f3390fdca
Jennifer King <jeking@andrew.cmu.edu>
2015-10-26 19:37:19 -0700

    Adding render flag. Removing uneeded 0.01 offset in goal height. Small code cleanup.

--- src/herbpy/action/pushing.py
+++ src/herbpy/action/pushing.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import logging, numpy
-from prpy.action import ActionMethod
+from prpy.action import ActionMethod, ActionError
 from prpy.planning.base import PlanningError
 logger = logging.getLogger('herbpy')
 
 @ActionMethod
 def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius, 
                       manip=None, max_plan_duration=30.0, 
-                      shortcut_time=3., **kw_args):
+                      shortcut_time=3., render=True, **kw_args):
     """
     @param robot The robot performing the push
     @param obj The object to push
@@ -18,25 +18,33 @@ def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius,
     @param manip The manipulator to use for the push - if None the active manipulator is used
     @param max_plan_duration The max time to run the planner
     @param shortcut_time The amount of time to spend shortcutting, if 0. no shortcutting is performed
+    @param render If true, render the trajectory while executing
     """
     # Get a push planner
-    from or_pushing.push_planner import PushPlanner
-    planner = PushPlanner(robot.GetEnv())
+    try:
+        from or_pushing.push_planner import PushPlanner
+        planner = PushPlanner(robot.GetEnv())
+    except ImportError:
+        raise ActionError("Unable to create PushPlanner. Is the randomized_rearrangement_planning"
+                          "repository checked out in your workspace?")
 
     # Get the manipulator
     if manip is None:
         with robot.GetEnv():
             manip = robot.GetActiveManipulator()
 
-    # Make the state bounds be at the edges of the table
+
     with robot.GetEnv():
         from prpy.rave import Disabled
         from prpy.util import ComputeEnabledAABB
         with Disabled(table, padding_only=True):
             table_aabb = ComputeEnabledAABB(table)
+        ee_pushing_transform = manip.GetEndEffectorTransform()
+        goal_pose = obj.GetTransform()
+
+    # Make the state bounds be at the edges of the table
     table_pos = table_aabb.pos()
     table_extents = table_aabb.extents()
-
     sbounds = {'high': [table_pos[0] + table_extents[0],
                         table_pos[1] + table_extents[1],
                         2.*numpy.pi],
@@ -44,16 +52,12 @@ def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius,
                        table_pos[1] - table_extents[1],
                        0]}
     
-    # Get the current pose of the manipulator and assume we want to keep that pose throughout
-    #  the push
-    with robot.GetEnv():
-        ee_pushing_transform = manip.GetEndEffectorTransform()
+    # Assume we want to keep the current orientation and height of the manipulator
+    #  throughout the push
     ee_pushing_transform[:2,3] = [0., 0.] #ignore x,y pose
 
     # Compute the goal pose
-    with robot.GetEnv():
-        goal_pose = obj.GetTransform()
-    table_height = table_pos[2] + table_extents[2] + 0.01
+    table_height = table_pos[2] + table_extents[2]
     goal_pose[:3,3] = [goal_position[0],
                        goal_position[1],
                        table_height]
@@ -70,10 +74,10 @@ def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius,
 
     # Execute
     from prpy.viz import RenderTrajectory
-    with RenderTrajectory(robot, traj, color=[1, 0, 0, 1]):
+    with RenderTrajectory(robot, traj, color=[1, 0, 0, 1], render=render):
         if shortcut_time > 0:
             traj = planner.ShortcutPath(timelimit=shortcut_time)
-        with RenderTrajectory(robot, traj, color=[0, 0, 1, 1]):
+        with RenderTrajectory(robot, traj, color=[0, 0, 1, 1], render=render):
           if manip.simulated:
               planner.ExecutePlannedPath()
           else:
