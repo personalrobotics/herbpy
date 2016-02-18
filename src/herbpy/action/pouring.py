9f54c80f6065a6ee863e90e3b0d50f7b1a99a18f
Jennifer King <jeking@andrew.cmu.edu>
2016-02-18 10:47:13 -0500

    Adding robot state savers

--- src/herbpy/action/pouring.py
+++ src/herbpy/action/pouring.py
@@ -29,15 +29,16 @@ def MoveCupAndPour(robot, table, manip_pitcher, cup, pitcher):
     cspec = traj.GetConfigurationSpecification()
     wpt = traj.GetWaypoint(traj.GetNumWaypoints() - 1)
     config = cspec.ExtractJointValues(wpt, robot, manip_pitcher.GetArmIndices())
-    with env, robot:
-        manip_pitcher.SetDOFValues(config)
-        desired_cup_in_world_x = pitcher_start[0,3] + (2*pitcher_aabb.extents()[0])
-        desired_cup_in_world_y = pitcher_start[1,3] - (2*pitcher_aabb.extents()[1])
-        cup_in_world_x = cup_pose[0, 3]
-        cup_in_world_y = cup_pose[1, 3]
-        move_x = desired_cup_in_world_x - cup_in_world_x + cup_aabb.extents()[0]/2
-        move_y = desired_cup_in_world_y - cup_in_world_y - cup_aabb.extents()[1]
-        move_cup = math.sqrt(move_x ** 2 + move_y ** 2)
+    with env:
+        with CreateRobotStateSaver( openravepy.KinBody.SaveParameters.LinkTransformation ):
+            manip_pitcher.SetDOFValues(config)
+            desired_cup_in_world_x = pitcher_start[0,3] + (2*pitcher_aabb.extents()[0])
+            desired_cup_in_world_y = pitcher_start[1,3] - (2*pitcher_aabb.extents()[1])
+            cup_in_world_x = cup_pose[0, 3]
+            cup_in_world_y = cup_pose[1, 3]
+            move_x = desired_cup_in_world_x - cup_in_world_x + cup_aabb.extents()[0]/2
+            move_y = desired_cup_in_world_y - cup_in_world_y - cup_aabb.extents()[1]
+            move_cup = math.sqrt(move_x ** 2 + move_y ** 2)
 
     from prpy.rave import Disabled
     with Disabled(table):
@@ -49,22 +50,16 @@ def MoveCupAndPour(robot, table, manip_pitcher, cup, pitcher):
         manip_cup.PlanToEndEffectorOffset(direction=-1*manip_cup_pose[:3,0], distance=0.1, position_tolerance=0.1, execute=True, timelimit=10)
         manip_cup.PlanToNamedConfiguration('home', execute=True)
 
-    v_original = manip_pitcher.GetVelocityLimits()
     # Pour
     # slow the arm down
     v = numpy.array([0.75, 0.75, 2., 2., 2.5, 2.5, 2.5])
-    try:
+    with CreateRobotStateSaver( openravepy.KinBody.SaveParameters.JointMaxVelocityAndAcceleration ):
         manip_pitcher.SetVelocityLimits(0.5*v, 0.3)
         
         #Gets position of the pitcher and pours
         traj = robot.PostProcessPath(traj)
         robot.ExecuteTrajectory(traj)
         time.sleep(1) #To let the water pour from pitcher into cup
-    
-        #Slows down the speed of the arm
-        manip_pitcher.SetVelocityLimits(v, 0.3)
-    finally:
-        manip_pitcher.SetVelocityLimits(v_original, 0.3)
 
     # Tilts pitcher back to original position
     robot.ExecuteTrajectory(openravepy.planningutils.ReverseTrajectory(traj))
