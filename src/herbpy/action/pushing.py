1897d2f2ebc95f2bb8179448e4fa24af1932c106
Jennifer King <jeking@andrew.cmu.edu>
2016-05-26 12:16:59 -0400

    Small updates to pushing action to match latest rearrangement planning master

--- src/herbpy/action/pushing.py
+++ src/herbpy/action/pushing.py
@@ -40,7 +40,6 @@ def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius,
         with Disabled(table, padding_only=True):
             table_aabb = ComputeEnabledAABB(table)
         ee_pushing_transform = manip.GetEndEffectorTransform()
-        goal_pose = obj.GetTransform()
 
     # Make the state bounds be at the edges of the table
     table_pos = table_aabb.pos()
@@ -58,16 +57,15 @@ def PushToPoseOnTable(robot, obj, table, goal_position, goal_radius,
 
     # Compute the goal pose
     table_height = table_pos[2] + table_extents[2]
-    goal_pose[:3,3] = [goal_position[0],
-                       goal_position[1],
-                       table_height]
+    goal_pose = [goal_position[0],
+                       goal_position[1]]
 
     with robot.CreateRobotStateSaver():
         traj = planner.PushToPose(robot, obj, goal_pose,
                                   state_bounds = sbounds,
                                   pushing_manifold = ee_pushing_transform.flatten().tolist(),
                                   max_plan_duration = max_plan_duration,
-                                  goal_epsilon = goal_radius,
+                                  goal_radius = goal_radius,
                                   **kw_args)
     if traj is None:
         raise PlanningError('Failed to find pushing plan')
