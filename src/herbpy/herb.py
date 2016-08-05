ab9b95c9bf782c124652e790d3a7b296aeb25aeb
Michael Koval <mkoval@cs.cmu.edu>
2016-08-05 13:11:55 -0400

    Removed more rosbuild legacy.

--- src/herbpy/herb.py
+++ src/herbpy/herb.py
@@ -1,17 +1,21 @@
-import logging, prpy, prpy.dependency_manager, os
+import logging
+import os
+import prpy
+import prpy.dependency_manager
 from openravepy import (
     Environment,
     RaveCreateModule,
     RaveInitialize,
 )
-from .herbbase import HerbBase
+from .herbbase import (
+    HerbBase,
+    HERBRobot,
+)
 
 logger = logging.getLogger('herbpy')
 
 def initialize(robot_xml=None, env_path=None, attach_viewer=False,
                sim=True, **kw_args):
-    import prpy, os
-
     prpy.logger.initialize_logging()
 
     # Hide TrajOpt logging.
@@ -27,35 +31,24 @@ def initialize(robot_xml=None, env_path=None, attach_viewer=False,
         if not env.Load(env_path):
             raise Exception('Unable to load environment frompath %s' % env_path)
 
-    if prpy.dependency_manager.is_catkin():
+    # Load the URDF file into OpenRAVE.
+    urdf_module = RaveCreateModule(env, 'urdf')
+    if urdf_module is None:
+        logger.error('Unable to load or_urdf module. Do you have or_urdf'
+                     ' built and installed in one of your Catkin workspaces?')
+        raise ValueError('Unable to load or_urdf plugin.')
 
-        # Load the URDF file into OpenRAVE.
-        urdf_module = RaveCreateModule(env, 'urdf')
-        if urdf_module is None:
-            logger.error('Unable to load or_urdf module. Do you have or_urdf'
-                         ' built and installed in one of your Catkin workspaces?')
-            raise ValueError('Unable to load or_urdf plugin.')
+    urdf_uri = 'package://herb_description/robots/herb.urdf'
+    srdf_uri = 'package://herb_description/robots/herb.srdf'
+    args = 'Load {:s} {:s}'.format(urdf_uri, srdf_uri)
+    herb_name = urdf_module.SendCommand(args)
+    if herb_name is None:
+        raise ValueError('Failed loading HERB model using or_urdf.')
 
-        urdf_uri = 'package://herb_description/robots/herb.urdf'
-        srdf_uri = 'package://herb_description/robots/herb.srdf'
-        args = 'Load {:s} {:s}'.format(urdf_uri, srdf_uri)
-        herb_name = urdf_module.SendCommand(args)
-        if herb_name is None:
-            raise ValueError('Failed loading HERB model using or_urdf.')
-
-        robot = env.GetRobot(herb_name)
-        if robot is None:
-            raise ValueError('Unable to find robot with name "{:s}".'.format(
-                             herb_name))
-    else:
-        if robot_xml is None:
-            import os, rospkg
-            rospack = rospkg.RosPack()
-            base_path = rospack.get_path('herb_description')
-            robot_xml = os.path.join(base_path, 'ordata', 'robots', 'herb.robot.xml')
-
-        robot = env.ReadRobotXMLFile(robot_xml)
-        env.Add(robot)
+    robot = env.GetRobot(herb_name)
+    if robot is None:
+        raise ValueError('Unable to find robot with name "{:s}".'.format(
+                         herb_name))
 
     # Default arguments.
     keys = [ 'left_arm_sim', 'left_hand_sim', 'left_ft_sim',
@@ -65,7 +58,6 @@ def initialize(robot_xml=None, env_path=None, attach_viewer=False,
         if key not in kw_args:
             kw_args[key] = sim
 
-    from herbrobot import HERBRobot
     prpy.bind_subclass(robot, HERBRobot, **kw_args)
 
     if sim:
